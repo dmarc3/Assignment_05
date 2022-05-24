3e74b38e9f97a0715ef22b27b58efbed0685b5b7
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-05-24 15:56:58 -0700

    Updated for users.

---
+++ socialnetwork_db.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+
+class MongoDBConnection():
+    '''MongoDB Connection'''
+
+    def __init__(self, host='127.0.0.1', port=27017):
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+
+def print_mdb_collection(collection_name):
+    [print(doc) for doc in collection_name.find()]
--- users.py
+++ users.py
@@ -4,84 +4,74 @@ All edits made by Kathleen Wong to incorporate logging issues.
 '''
 # pylint: disable=R0903
 import logging
+import pymongo
+import ipdb
 
 
-class Users():
-    '''
-    Contains user information
-    '''
-
-    def __init__(self, user_id, email, user_name, user_last_name):
-        logging.info('%s status initialized.', user_id)
-        self.user_id = user_id
-        self.email = email
-        self.user_name = user_name
-        self.user_last_name = user_last_name
-
-
-class UserCollection():
+class UserCollection:
     '''
     Contains a collection of Users objects
     '''
 
-    def __init__(self):
+    def __init__(self, mongo):
         logging.info('UserCollection initialized.')
-        self.database = {}
+        self.mongo = mongo
+        db = self.mongo.connection.media
+        self.database = db['UserAccounts']
+        self.database.create_index('user_id', unique=True)
+        self.database.create_index('user_email')
+        self.database.create_index('user_name')
+        self.database.create_index('user_last_name')
 
     def add_user(self, user_id, email, user_name, user_last_name):
         '''
         Adds a new user to the collection
         '''
-        if user_id in self.database:
-            # Rejects new status if user_id already exists
-            logging.error('Unable to modify %s because it ' \
-                          'does not exist in UserCollection.', user_id)
-            logging.debug("UserCollection contains following users: %s",
-                          ', '.join(self.database.keys()))
+        try:
+            new_user = {user_id: user_id, user_name: user_name, user_last_name: user_last_name, email: email}
+            self.database.insert_one(new_user)
+            logging.info('Successfully added user %s!', user_id)
+            return True
+        except pymongo.errors.DuplicateKeyError as exc:
+            logging.error('pymongo DuplicateKeyError encountered.')
+            logging.error(exc.details['errmsg'])
             return False
-        new_user = Users(user_id, email, user_name, user_last_name)
-        self.database[user_id] = new_user
-        logging.info('Successfully added user %s!', user_id)
-        return True
 
     def modify_user(self, user_id, email, user_name, user_last_name):
         '''
         Modifies an existing user
         '''
-        if user_id not in self.database:
-            logging.error('Unable to modify %s because it ' \
-                          'does not exist in UserCollection.', user_id)
-            logging.debug("UserCollection contains following users': %s",
-                          ', '.join(self.database.keys()))
+        try:
+            success = self.database.update_one({'user_id': user_id},
+                                               {'$set': {'user_email': email,
+                                                         'user_name': user_name,
+                                                         'user_last_name': user_last_name}})
+            logging.info('Updated %s.', user_id)
+            return success
+        except pymongo.errors.DuplicateKeyError as exc:
+            logging.error('pymongo DuplicateKeyError encountered.')
+            logging.error(exc.details['errmsg'])
             return False
-        self.database[user_id].email = email
-        self.database[user_id].user_name = user_name
-        self.database[user_id].user_last_name = user_last_name
-        logging.info('Successfully modified user %s!', user_id)
-        return True
 
     def delete_user(self, user_id):
         '''
         Deletes an existing user
         '''
-        if user_id not in self.database:
-            # Fails if status does not exist
-            logging.error('Unable to delete %s because it '
-                          'does not exist in UserCollection.', user_id)
-            logging.debug("UserCollection contains following user': %s,",
-                          ', '.join(self.database.keys()))
+        result = self.database.delete_one(dict(user_id=user_id))
+        if result.raw_result['n'] == 1:
+            logging.info('Deleted user %s.', user_id)
+            return True
+        else:
+            logging.error('Unable to delete %s. User does not exist.', user_id)
             return False
-        del self.database[user_id]
-        logging.info('Successfully deleted user %s!', user_id)
-        return True
 
     def search_user(self, user_id):
         '''
         Searches for user data
         '''
-        if user_id not in self.database:
-            logging.error('Unable to find %s in UserCollection.', user_id)
-            logging.debug("UserCollection contains following users': %s",
-                          ', '.join(self.database.keys()))
-            return Users(None, None, None, None)
-        return self.database[user_id]
+        user = self.database.find_one(dict(user_id=user_id))
+        if user:
+            logging.info('Found user %s.', user_id)
+        else:
+            logging.info('User %s not found.', user_id)
+        return user
